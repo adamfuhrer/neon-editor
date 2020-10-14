fdcb11999160d80efa8cd189ac6258e9a9d086f8
adamfuhrer <adam.fuhrer@caseware.com>
2020-10-14 11:20:31 -0400

    Add url redirect for wildcard route

--- app/components/editor.js
+++ app/components/editor.js
@@ -3,29 +3,29 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class EditorComponent extends Component {
-    @service('editor') editor;
+	@service('editor') editor;
 
-    @action onKeyUp(event) {
-        this.editor.setText(event.target.value);
-    }
+	@action onKeyUp(event) {
+		this.editor.setText(event.target.value);
+	}
 
-    @action onKeyDown(event) {
-        if (event.key && event.key.toLowerCase() === 'tab') {
-            // insert tab character into textarea and prevent default
-            this.editor.setText(event.target.value + `\t`)
-            event.preventDefault();
-        }
-    }
+	@action onKeyDown(event) {
+		if (event.key && event.key.toLowerCase() === 'tab') {
+			// insert tab character into textarea and prevent default behaviour
+			this.editor.setText(event.target.value + '\t');
+			event.preventDefault();
+		}
+	}
 
-    @action onClearClicked() {
-        this.editor.clearText();
-    }
+	@action onClearClicked() {
+		this.editor.clearText();
+	}
 
-    @action setSelectedColor(color) {
-        this.editor.setColor(color);
-    }
+	@action setSelectedColor(color) {
+		this.editor.setColor(color);
+	}
 
-    @action toggleFullScreen() {
-        this.editor.toggleFullScreen();
-    }
+	@action toggleFullScreen() {
+		this.editor.toggleFullScreen();
+	}
 }
--- app/router.js
+++ app/router.js
@@ -7,4 +7,6 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
+  this.route('index', { path: '/' });
+  this.route('not-found', { path: '/*path' });
 });
